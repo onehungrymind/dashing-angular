7feb9b9765ab74ea7baae0b9394ab6860604ae7b
Lukas Ruebbelke <simpul@gmail.com>
2017-05-14 06:58:20 -0700

    update: templates solution

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
+  selectedClient;
+  clients = [
+    {
+      'id': 1,
+      'img': 'assets/user.jpg',
+      'name': 'John Doe',
+      'description': 'Going places'
+    },
+    {
+      'id': 2,
+      'img': 'assets/user.jpg',
+      'name': 'Jane Smith',
+      'description': 'Risk averse'
+    },
+    {
+      'id': 3,
+      'img': 'assets/user.jpg',
+      'name': 'Matt Jones',
+      'description': 'High roller',
+    }];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectClient(client) {
+    this.selectedClient = client;
+  }
 }
