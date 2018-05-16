7311ec664f21b5fb8f5bbd57f499dbc0aea0eaa8
Garvey, Jonathan <Jonathan.Garvey@aa.com>
2018-05-15 17:56:21 -0700

    Upgrade to latest everything

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,7 +25,7 @@ module.exports = function (config) {
       'text/x-typescript': ['ts','tsx']
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
--- src/app/common/reducers/index.ts
+++ src/app/common/reducers/index.ts
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { ActionReducer, combineReducers } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, combineReducers, MetaReducer } from '@ngrx/store';
 import { compose } from '@ngrx/core/compose';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../../environments/environment';
@@ -16,6 +16,7 @@ export interface State {
   symbols: symbols.State;
 }
 
+export
 const reducers = {
   portfolios: portfolios.reducer,
   stocks: stocks.reducer,
@@ -23,16 +24,9 @@ const reducers = {
   symbols: symbols.reducer
 };
 
-const developmentReducer: ActionReducer<any> = compose(storeFreeze, combineReducers)(reducers);
-const productionReducer: ActionReducer<any> = combineReducers(reducers);
-
-export function reducer(state: any, action: any) {
-  if (environment.production) {
-    return productionReducer(state, action);
-  } else {
-    return developmentReducer(state, action);
-  }
-}
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? [storeFreeze]
+  : [];
 
 // -------------------------------------------------------------------
 // Portfolio Selectors
